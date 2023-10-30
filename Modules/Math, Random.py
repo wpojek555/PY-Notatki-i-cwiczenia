ba7da6d64b749492cc81bf30e3e614e6a2e2d9e0
wpojek555 <wpojek555@gmail.com>
2023-10-30 20:13:15 +0100

    Modules

---
+++ Modules/Math, Random.py
@@ -0,0 +1,9 @@
+from math import pi
+from math import sqrt as pierwiastek
+import random
+
+print("pi: ", pi)
+
+print("Losowa liczba od 1 do 100:", random.randint(1, 100))
+
+print("Pierwiastek z 144:", pierwiastek(144))
---
+++ Modules/Time.py
@@ -0,0 +1,5 @@
+import time
+
+for x in range(10):
+    print(x + 1)
+    time.sleep(1)
